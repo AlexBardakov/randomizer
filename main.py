b8b322b7f5ba9b406a9cf97178f26969dca6aa14
KompukterMl <Battleover@yandex.ru>
2023-07-25 16:02:12 +0700

    Сделал вывод списка + считывание клавиши enter

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox as msg
+import keyboard
+import random
 
-clicks = 0
-new_name = ""
-name_list = []
+new_name = ""  # переменная для нового имени
+name_list = []  # итоговый список имен
 
-def click_button():
+
+def enter_press():
+    add_new()
+
+
+def stop_button():
     global new_name
     new_name = entry.get()
     name_list.append(new_name)
     entry.delete(0, END)
+    lbox.delete(0, END)
     entry.insert(0, "Вывожу список")
+    btn_stop["state"] = "disabled"
     for i in name_list:
-        print(i)
+        lbox.insert(0, i)
+
 
 def add_new():
     global new_name
     new_name = entry.get()
     name_list.append(new_name)
     entry.delete(0, END)
+    lbox.delete(0, END)
+    btn_stop["state"] = "enabled"
+    for i in name_list:
+        lbox.insert(0, i)
 
 
 root = Tk()
 root.title("Турнирная сетка")
-root.geometry("250x200")
+root.geometry("200x200")
 
 entry = ttk.Entry()
 entry.pack(padx=6, pady=6)
 
-btn_stop = ttk.Button(text="Стоп", command=click_button)
+btn_stop = ttk.Button(text="Стоп", command=stop_button)
 btn_stop.pack()
 btn_add = ttk.Button(text="Добавить", command=add_new)
 btn_add.pack()
 
-root.mainloop()
+lbox = Listbox(width=15, height=5)
+lbox.pack()
 
+keyboard.add_hotkey("enter", enter_press)
+
+
+root.mainloop()
